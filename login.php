ffcb7ceff97c23d0f2466dc08201723e9424704f
adtrex <you@example.com>
2020-04-09 20:56:20 +0100

    update

---
+++ login.php
@@ -0,0 +1,57 @@
+<?php session_start();
+
+include_once('header.php');
+
+?>
+
+
+
+	<p>
+		<?php
+			
+					if(isset($_SESSION['message']) && !empty($_SESSION["message"])){
+						echo "<span>" . $_SESSION['message']. "</span>";
+						//session_unset();
+						session_destroy();
+					}
+
+		?>
+	</p>
+
+	Login form Here
+
+	<form method="POST" action="processregister.php">
+
+			<p>
+				<?php
+
+					if(isset($_SESSION['error']) && !empty($_SESSION["error"])){
+						echo "<span style='color:red'>" . $_SESSION['error']. "</span>";
+						//session_unset();
+						session_destroy();
+					}
+				?>
+			</p>
+			
+			
+				<label>Email</label><br />
+				<input 
+
+				<?php
+				
+				if(isset($_SESSION['email'])){
+							echo "value=" . $_SESSION['email'];
+				}
+
+				?>
+
+				type="text" name="email" placeholder="Email"  />
+			</p>
+			<p>
+				<label>Password</label><br />
+				<input 
+
+				type="text" name="password" placeholder="Password"  />
+			</p>
+
+<?php include_once('footer.php'); ?>
